1f5a2eb8a03ff4d489b0377d02985d6e527ba95a
Csrhdz <105759540+Csrhdz@users.noreply.github.com>
2024-05-28 17:12:29 +0000

    actualizar

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
-
-
 // Inicializa EmailJS con Public Key
 (function(){
-    emailjs.init("GIigsFAhMFrRS_anK"); // Reemplaza "YOUR_PUBLIC_KEY" con tu clave pública de EmailJS
+    emailjs.init("1Krai8GWhxZXbmunXrEj6"); // Reemplaza "1Krai8GWhxZXbmunXrEj6" con tu clave pública de EmailJS
 })();
 
 document.getElementById('infoForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
+    event.preventDefault(); // Previene el comportamiento por defecto del formulario
+
     // Recoge los datos del formulario
     const nombre = document.getElementById('nombre').value;
     const telefono = document.getElementById('telefono').value;
     const servicio = document.getElementById('servicio').value;
     const mensaje = document.getElementById('mensaje').value;
-    
+
     // Envía el formulario a través de EmailJS
     emailjs.send("service_12g7xgl", "template_133diss", {
         nombre: nombre,
@@ -24,9 +22,9 @@ document.getElementById('infoForm').addEventListener('submit', function(event) {
     .then(function(response) {
         console.log('SUCCESS!', response.status, response.text);
         alert('Formulario enviado con éxito!');
+        document.getElementById('infoForm').reset(); // Resetea el formulario
     }, function(error) {
-        console.error('FAILED...', error); // Usa console.error para errores
+        console.error('FAILED...', error);
         alert('Error al enviar el formulario. Por favor, inténtelo de nuevo.');
     });
-    
 });
